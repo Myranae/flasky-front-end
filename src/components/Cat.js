6dddb73ae520fee80f69a569e7170802f6b1cf34
Myranae <myranae@pm.me>
2022-06-14 13:22:27 -0400

    goes through practice during roundtable

---
+++ src/components/Cat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cat = () => {
+    return (
+        <li>
+            <h3>Name of Cat</h3>
+            <button>Pet Cat</button>
+        </li>
+    );
+};
+
+export default Cat;
---
+++ src/components/CatList.js
@@ -0,0 +1,13 @@
+import Cat from "./Cat";
+
+const CatList = () => {
+    return (
+        <ul>
+            <Cat />
+            <Cat />
+            <Cat />ß
+        </ul>
+    );
+};
+
+export default CatList;
