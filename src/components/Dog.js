5d8988fd29e257218701e614efe62072edcf9089
Myranae <myranae@pm.me>
2022-06-15 13:48:22 -0400

    updates Dog and adds data

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,12 +1,21 @@
 import "./Dog.css";
+import PropTypes from "prop-types";
 
-const Dog = () => {
+const Dog = (props) => {
     return (
         <li className="dog-list__li">
-            <h3>Name of Dog</h3>
+            <h3>{props.name}</h3>
+            <h4>{props.favoriteToy}</h4>
+            <h4>{props.chipNumber}</h4>
             <button>Pet Dog</button>
         </li>
     );
+};
+
+Dog.propTypes = {
+    name: PropTypes.string.isRequired,
+    favoriteToy: PropTypes.string.isRequired,
+    chipNumber: PropTypes.number.isRequired,
 };
 
 export default Dog;
--- src/components/DogList.js
+++ src/components/DogList.js
@@ -2,6 +2,26 @@ import Dog from "./Dog";
 import "./DogList.css";
 
 const DogList = () => {
+    const dog_data = [
+        {
+          name: "Wishbone",
+          favoriteToy: "Squeaker",
+          chipNumber: "82373",
+        },
+        {
+          name: "Appa",
+          favoriteToy: "Ball",
+          chipNumber: "29238",
+        },
+        {
+          name: "Jimminy",
+          favoriteToy: "Bone",
+          chipNumber: "72902",
+        },
+       ];
+
+    const dogCOmponent
+
     return (
         <ul>
             <Dog />
